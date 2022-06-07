45671668a2008381e3012b89ff2f7f380ad869d3
Hasibur Rahman <hasibur.rahman@mail.utoronto.ca>
2022-06-07 16:33:18 +0600

    Delete src directory

--- src/app.js
+++
@@ -1,8 +0,0 @@
-const burgerButton = document.getElementById('burger')
-const navList = document.getElementById('nav-list')
-
-function toggleButton(){
-    navList.classList.toggle('show')
-}
-
-burgerButton.addEventListener('click', toggleButton)
